6324bce582a977d3a12d3e5f1092982183fc5743
Philippe Willot <willot.ph@gmail.com>
2022-06-13 20:40:34 +0200

    Adding get, delete and put routes for users

--- server/express/routes/users.js
+++ server/express/routes/users.js
@@ -19,6 +19,35 @@ userRouter.use('/:id', async (req, res, next) => {
   }
 });
 
+// Update an user dynamically
+userRouter.use('/:id/*', async (req, res, next) => {
+  const user = req.user;
+  const dataToUpdate = req.params[0];
+
+  if (!(dataToUpdate in req.body)) {
+    return res
+      .status(400)
+      .send({ status: `missing ${dataToUpdate} in the body` });
+  }
+
+  if (user.dataValues[dataToUpdate] === req.body[dataToUpdate]) {
+    return res
+      .status(400)
+      .send({ status: `You can't use your previous ${dataToUpdate}` });
+  } else {
+    try {
+      const dataObject = {};
+      dataObject[dataToUpdate] = req.body[dataToUpdate];
+      await models.users.update(dataObject, {
+        where: { id: req.params.id },
+      });
+    } catch (err) {
+      console.error(err);
+    }
+    next();
+  }
+});
+
 userRouter.get('/', async (req, res, next) => {
   res.send({ users: await models.users.findAll() });
 });
@@ -27,20 +56,25 @@ userRouter.get('/:id', (req, res, next) => {
   res.send({ user: req.user });
 });
 
-userRouter.put('/:id', (req, res, next) => {
-  if (req.body.password || req.body.city) {
-  } else {
-    res.status(400).send({ status: 'Body needs a new city or a password' });
-  }
+userRouter.put('/:id/password', (req, res, next) => {
+  res.status(200).send();
+});
+
+userRouter.put('/:id/city', (req, res, next) => {
+  res.status(200).send();
 });
 
 userRouter.delete('/:id', async (req, res, next) => {
-  await models.users.destroy({
-    where: {
-      id: req.params.id,
-    },
-  });
-  res.status(204).send();
+  try {
+    await models.users.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(204).send();
+  } catch (err) {
+    console.error(err);
+  }
 });
 
 module.exports = userRouter;
