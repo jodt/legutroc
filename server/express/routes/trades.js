56b4d8d0d12676f7c26a74dfcf7f2f01fc741492
Philippe Willot <willot.ph@gmail.com>
2022-06-18 14:19:22 +0200

    Refactoring code and starting trades routes

--- server/express/routes/trades.js
+++ server/express/routes/trades.js
@@ -3,4 +3,20 @@ const { models } = require('../../sequelize');
 
 const tradeRouter = express.Router();
 
+tradeRouter.get('/', async (req, res, next) => {
+  res.status(200).send({ trades: await models.trades.findAll() });
+});
+
+tradeRouter.post('/', async (req, res, next) => {
+  try {
+    await models.trades.create({
+      userProductionId_1: req.body.userProductionId_1,
+      userProductionId_2: req.body.userProductionId_1,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send();
+  }
+});
+
 module.exports = tradeRouter;
